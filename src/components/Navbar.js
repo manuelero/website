a6629be8b71bbc5d565c4dc21a3b7c9405748366
chopan123 <chopancatrileo@gmail.com>
2022-04-08 17:07:11 -0400

    Navbar showing always

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,14 +5,8 @@ import discord from '../assets/discord.png'
 const Navbar = () => {
 	return (
 		<div className="navbar">
-			<a className="discord_icon" target="_blank" rel="noreferrer" href="https://discord.gg/S5mM2BjsTp">
-			<img src={discord} alt="discord" />
-			</a>
-			<a className="twitter_icon" target="_blank" rel="noreferrer" href="https://twitter.com/ShapeMonsters">
-				<img src={TwitterImg} alt="twitter" />
-			</a>
 
-			<button className="wallet_btn">connect wallet</button>
+			<button className="menu_btn"> BR4 </button>
 		</div>
 	)
 }
