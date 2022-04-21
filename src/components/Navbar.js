d330fc91a406ac4d46b597ec5e959c92fb4ff970
chopan123 <chopancatrileo@gmail.com>
2022-04-21 15:47:52 -0400

    Navbar with logo, distressed assets done

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,14 @@
 import TwitterImg from '../assets/TwitterImg.png'
 import discord from '../assets/discord.png'
-
+import Menu from '../assets/menu.png'
 
 const Navbar = () => {
 	return (
 		<div className="navbar">
 
-			<button className="menu_btn"> BR4 </button>
+			<button className="menu_btn"> 
+				<img src={Menu}/>
+			</button>
 		</div>
 	)
 }
